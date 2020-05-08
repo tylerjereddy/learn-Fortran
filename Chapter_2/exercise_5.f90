6279619dd48ad64072d51b4c9c4d0e43e757c98d
Tyler Reddy <tyler.je.reddy@gmail.com>
2020-05-08 16:56:55 -0600

    Add Chapter 2, Exercise 5, variable b work.

--- Chapter_2/exercise_5.f90
+++ Chapter_2/exercise_5.f90
@@ -1,9 +1,15 @@
       subroutine array_work
-          real, dimension(11)     :: a
+          real, dimension(11)        :: a
+          real, dimension(0:11)      :: b
 
           write(*, *) "a(1):", a(1)
           write(*, *) "a(10):", a(10)
           write(*, *) "a(11):", a(11)
+
+          write(*, *) "b(0):", b(0)
+          write(*, *) "b(10):", b(10)
+          write(*, *) "b(11):", b(11)
+
       end subroutine array_work
 
       program x
