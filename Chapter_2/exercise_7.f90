9e639d520d2c75223d41ec76ecce530c73f97c74
Tyler Reddy <tyler.je.reddy@gmail.com>
2020-05-22 15:06:34 -0600

    Add Exercise 7, part ii)

--- Chapter_2/exercise_7.f90
+++ Chapter_2/exercise_7.f90
@@ -1,6 +1,7 @@
       subroutine learning_derived_types
           implicit none
 
+          ! type declarations
           type vehicle_registration
 
               character(len=20) :: color
@@ -11,8 +12,16 @@
 
           end type vehicle_registration
 
-          type(vehicle_registration) :: tyler_registration
+           type circle
+               real :: radius
+               real, dimension(3) :: center
+           end type circle
 
+          real, dimension(3) :: my_center
+          type(vehicle_registration) :: tyler_registration
+          type(circle) :: my_circle
+
+          ! exercise the derived types from above
           tyler_registration = vehicle_registration('blue', &
                                                     'Ford', &
                                                     'Mustang', &
@@ -25,6 +34,18 @@
                         tyler_registration%model_name, &
                         tyler_registration%model_year, &
                         tyler_registration%registration_year
+
+
+           my_center(1) = 0.0
+           my_center(2) = 1.0
+           my_center(3) = 2.0
+
+           my_circle = circle(3.00718789, my_center)
+
+           write(*, *) "my_circle components:", &
+                       "radius:", my_circle%radius, &
+                       "center:", my_circle%center
+
 
 
       end subroutine learning_derived_types
