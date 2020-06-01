3ba131595228c38850bf9a00e8c21c0a4568fc63
Tyler Reddy <tyler.je.reddy@gmail.com>
2020-06-01 16:06:03 -0600

    Add Chapter 2, Exercise 9, part b).

--- Chapter_2/exercise_9.f90
+++ Chapter_2/exercise_9.f90
@@ -3,8 +3,12 @@
 
           ! integer variable inside the range -10**20 and 10**20
           integer, parameter :: k20 = selected_int_kind(20)
+          ! a real variable with a minimum of 12 decimal digits and
+          ! of precision and range of 10**-100 to 10**100
+          integer, parameter :: long = selected_real_kind(12, 100)
 
           write(*, *) "part a) integer range:", range(-17_k20)
+          write(*, *) "part b) real range:", range(1.7_long)
 
       end subroutine entity_specifications
 
